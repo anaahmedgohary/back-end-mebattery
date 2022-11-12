2d390bd62ed4e86626531ae07a8acf566a919ffc
anaahmedgohary <gogoahmed13@gmail.com>
2022-11-13 00:17:42 +0200

    router03

--- index.js
+++ index.js
@@ -52,77 +52,23 @@ app.use(bodyParser.json())
     // Requests
 
     app.get("/fine", (req, res) =>
-{
+    {
     let showes = ["aaaaa", "qqqqqqqq"];
     
     res.json(showes);
 
     })
 
-app.get("/savednotes", (req, res) =>
-    {
-        let sqlCommand = "SELECT * FROM mebattery";
+    
 
-        db.query(sqlCommand, (err, result) =>
-        {
-            if (err) { throw err };
-            console.log(result);
-            // res.send(JSON.stringify(result));
-            res.send(result)
-        })
-    })
 
-app.post("/commentlevel", (req, res) =>
-    {
-        let sqlCommand = "INSERT INTO mebattery SET ?";
-
-        let reqBody = req.body;
-        let comment = reqBody.comment;
-        let level = reqBody.level;
-
-        let theData = { comment: `${comment}`, level: `${level}` };
-
-        db.query(sqlCommand, theData, (err, result) =>
-        {
-            if (err)
-            {
-                throw err;
-                res.send("problems faced along the way! \nPlease try again.");
-            };
-            console.log(result);
-            res.send("New Comment Saved Successfully");
-        })
-    })
-
-    .post("/createtable",
-        (req, res) =>
-        {
-            let sql = "CREATE TABLE mebattery(id int Auto_increment, date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, comment VARCHAR(255), level VARCHAR(5), PRIMARY KEY(id))";
-
-            db.query(sql, (err, result) =>
-            {
-                if (err) { throw err };
-                console.log(result);
-                res.send("Table Created! Success")
-            })
-        }
-    )
-
-    .get("/routerpost", (req, res) =>
-    {
-        let sql = 'INSERT INTO mebattery SET ?';
-        let post = { comment: "routercomment", level: 'cook' };
-
-        db.query(sql, post, (err, result) =>
-        {
-            if (err) { throw err };
-            console.log(result);
-            res.send('inserted into tabel post');
-        })
-    })
 
     
 
+    
+
+    
+// portfolio messager from netlify
 app.post("/portfoliomessage", async (req, res) =>
     {
         let sqlcommand = "INSERT INTO portfoliomsg SET ?";
@@ -141,7 +87,7 @@ app.post("/portfoliomessage", async (req, res) =>
         {
             if (err) { throw err; };
             console.log(result);
-            res.json("req.body.name");
+            res.json("Thank You.\nI'll get back to you ASAP.");
         })
 
 
--- routes/handler.js
+++ routes/handler.js
@@ -19,41 +19,103 @@ router
     .get('/', async (req, res) =>
     {
         let obj = { name: "isname", age: "isage" };
-        let arrofobj = [{man:"iuy"},{man:"qwe"}]
+        let arrofobj = [{ man: "iuy" }, { man: "qwe" }]
 
         // res.send(JSON.stringify(obj));
         try
         {
-           // res.status(200).send(arrofobj);
+            // res.status(200).send(arrofobj);
             console.log("tweet sucess");
-           // let body = await res.data.body.name;
+            // let body = await res.data.body.name;
             res.json({
                 status: 200,
                 body: obj,
                 data: arrofobj
-          });
-            
-           // console.log(body)
+            });
+
+            // console.log(body)
 
         }
-      catch (error) {
-          console.log(error)
+        catch (error)
+        {
+            console.log(error)
         }
 
 
     })
 
-    
+    .get("/routerpost", (req, res) =>
+    {
+        let sql = 'INSERT INTO mebattery SET ?';
+        let post = { comment: "routercomment", level: 'cook' };
 
-    
+        db.query(sql, post, (err, result) =>
+        {
+            if (err) { throw err };
+            console.log(result);
+            res.send('inserted into tabel post');
+        })
+    })
 
-   
-    
+    .post("/createtable",
+        (req, res) =>
+        {
+            let sql = "CREATE TABLE mebattery(id int Auto_increment, date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, comment VARCHAR(255), level VARCHAR(5), PRIMARY KEY(id))";
 
-    
+            db.query(sql, (err, result) =>
+            {
+                if (err) { throw err };
+                console.log(result);
+                res.send("Table Created! Success")
+            })
+        }
+    )
 
-    // portfolio messager from netlify
-    
+    .post("/commentlevel", (req, res) =>
+    {
+        let sqlCommand = "INSERT INTO mebattery SET ?";
+
+        let reqBody = req.body;
+        let comment = reqBody.comment;
+        let level = reqBody.level;
+
+        let theData = { comment: `${comment}`, level: `${level}` };
+
+        db.query(sqlCommand, theData, (err, result) =>
+        {
+            if (err)
+            {
+                throw err;
+                res.send("problems faced along the way! \nPlease try again later.");
+            };
+            console.log(result);
+            res.send("New Comment Saved Successfully");
+        })
+    })
+
+    .get("/savednotes", (req, res) =>
+    {
+        let sqlCommand = "SELECT * FROM mebattery";
+
+        db.query(sqlCommand, (err, result) =>
+        {
+            if (err) { throw err };
+            console.log(result);
+            // res.send(JSON.stringify(result));
+            res.send(result)
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
 
 
 
