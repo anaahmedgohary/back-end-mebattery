4facdcf8d6fdb9b8f36a9457e7775a45183304ca
anaahmedgohary <gogoahmed13@gmail.com>
2022-11-12 21:04:29 +0200

    aaw

--- routes/handler.js
+++ routes/handler.js
@@ -43,80 +43,14 @@ router
 
     })
 
-    .post("/createtable",
-        (req, res) =>
-        {
-            let sql = "CREATE TABLE mebattery(id int Auto_increment, date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, comment VARCHAR(255), level VARCHAR(5), PRIMARY KEY(id))";
+    
 
-            db.query(sql, (err, result) =>
-            {
-                if (err) { throw err };
-                console.log(result);
-                res.send("Table Created! Success")
-            })
-        }
-    )
+    
 
-    .get("/insertlog", (req, res) =>
-    {
-        let sql = 'INSERT INTO mebattery SET ?';
-        let post = { comment: "firstcomment", level: 99 };
+   
+    
 
-        db.query(sql, post, (err, result) =>
-        {
-            if (err) { throw err };
-            console.log(result);
-            res.send('inserted into tabel post');
-        })
-    })    
-
-    .get("/routerpost", (req, res) =>
-    {
-        let sql = 'INSERT INTO mebattery SET ?';
-        let post = { comment: "routercomment", level: 'cook' };
-
-        db.query(sql, post, (err, result) =>
-        {
-            if (err) { throw err };
-            console.log(result);
-            res.send('inserted into tabel post');
-        })
-    })
-
-    .post("/commentlevel", (req, res) =>
-    {
-        let sqlCommand = "INSERT INTO mebattery SET ?";
-
-        let reqBody = req.body;
-        let comment = reqBody.comment;
-        let level = reqBody.level;
-
-        let theData = { comment: `${comment}`, level: `${level}` };
-
-        db.query(sqlCommand, theData, (err, result) =>
-        {
-            if (err)
-            {
-                throw err;
-                res.send("problems faced along the way! \nPlease try again.");
-            };
-            console.log(result);
-            res.send("New Comment Saved Successfully");
-        })
-    })
-
-    .get("/savednotes", (req, res) =>
-    {
-        let sqlCommand = "SELECT * FROM mebattery";
-
-        db.query(sqlCommand, (err, result) =>
-        {
-            if (err) { throw err };
-            console.log(result);
-           // res.send(JSON.stringify(result));
-           res.send(result)
-        })
-    })
+    
 
     // portfolio messager from netlify
     
